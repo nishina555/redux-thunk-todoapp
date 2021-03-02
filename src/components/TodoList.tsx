965c8a09815af2909959cc2e7d8e3a4f7dca4c32
nishina555 <n.toshi.1.014@gmail.com>
2021-03-02 21:53:36 +0900

    useEffectで非同期処理をする

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -4,6 +4,7 @@ import { setTodos, TodoActions } from "../redux/actions";
 import { RootState } from "../redux/types";
 import { connect } from "react-redux";
 import { TodoItem } from "../redux/types";
+import axios from "axios";
 
 type TodoListProps = {
   todos: Array<TodoItem>;
@@ -12,16 +13,20 @@ type TodoListProps = {
 
 const TodoList: React.FC<TodoListProps> = ({ todos, setTodos }) => {
   useEffect(() => {
-    const data: Array<TodoItem> = [
-      { id: 1, content: "do something", completed: false },
-      { id: 2, content: "go somewhere", completed: false },
-    ];
-    setTodos(data);
-  }, [setTodos]);
+    const fetchTodos = async () => {
+      const response = await axios
+        .get(`http://localhost:4000/todos`)
+        .catch((error) => {
+          throw new Error(error.message);
+        });
+      setTodos(response.data);
+    };
+    fetchTodos();
+  }, []);
   return (
     <ul className="todo-list">
       {todos && todos.length
-        ? todos.map((todo: any, index: any) => {
+        ? todos.map((todo: TodoItem, index: number) => {
             return <Todo key={`todo-${todo.id}`} todo={todo} />;
           })
         : "No todos, yay!"}
